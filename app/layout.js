5d57b9ecccf35a67e9e6d9087019980854b8cc9f
AmarJeetSharma6802 <amarsharma6802@gmail.com>
2025-03-21 19:56:20 +0530

    update favicon link to a new image source

--- app/layout.js
+++ app/layout.js
@@ -25,7 +25,7 @@ export default function RootLayout({ children }) {
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
         />
-        <link rel="icon" href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVgaeV7d7Zv9YyyRzK8RiM-KxkZW1Z5aHyMA&s" />
+        <link rel="icon" href="https://cdn-icons-png.flaticon.com/512/5229/5229345.png" />
 
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
