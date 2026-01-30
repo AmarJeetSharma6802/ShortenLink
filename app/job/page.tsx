e2731a0aa32c947372fd41dc04cb030d70482955
AmarJeetSharma6802 <amarsharma6802@gmail.com>
2026-01-30 19:14:15 +0530

    remove promise

--- app/job/page.tsx
+++ app/job/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import {  useRouter } from "next/navigation";
-import { promises } from "dns";
 
 interface job{
     _id: string;
